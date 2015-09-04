06f769ecf36b1961555aa76e055d7b296337aa45
huangsen0912 <1417988198@qq.com>
2015-09-04 19:34:33 +0800

    with  all()

--- yii2test/frontend/models/Customer.php
+++ yii2test/frontend/models/Customer.php
@@ -4,8 +4,8 @@ use yii\db\ActiveRecord;
 
 class Customer extends ActiveRecord{
 		public function getOrders(){
-
-			$orders= $this->hasMany(Order::className(),array('cid'=>'id'))->asArray()->all();
+			//with用法，先不用all查询
+			$orders= $this->hasMany(Order::className(),array('cid'=>'id'))->asArray(); //->all();
 			return $orders;
 		}
 }
