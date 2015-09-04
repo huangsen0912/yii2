4cf74b77667957474eb4014d7f6a9ca85bca99f2
huangsen0912 <1417988198@qq.com>
2015-09-04 21:31:15 +0800

    缓存设置

--- yii2test/frontend/controllers/CacheController.php
+++ yii2test/frontend/controllers/CacheController.php
@@ -18,13 +18,14 @@ class CacheController extends Controller{
 		$cache=\YII::$app->cache; //cache->getCache()方法->方法中加载cache组件->cache文件
 		//添加数据
 		//$cache->add('key1','helloworld');
-		$cache->add('key1',"zhidao"); //往缓存中添加，如果键相同，那么添加的数据不会覆盖
+		//$cache->add('key1',"zhidao"); //往缓存中添加，如果键相同，那么添加的数据不会覆盖
 		//修改数据
-		$cache->set('key1','hello world2');
+		//缓存设置，设置15秒
+		//$cache->set('key1','hello world2',15);
 		//删除数据
-		$cache->delete('key1');
+		//$cache->delete('key1');
 		//清空数据
-		$cache->flush();
+		//$cache->flush();
 		//调出数据
 		var_dump($cache->get('key1'));
 
