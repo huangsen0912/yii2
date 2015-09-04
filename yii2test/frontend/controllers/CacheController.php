fc6075484bc506d5a70929155a8e5937ecba8f4f
huangsen0912 <1417988198@qq.com>
2015-09-04 21:22:31 +0800

    cache 缓存基本操作

---
+++ yii2test/frontend/controllers/CacheController.php
@@ -0,0 +1,36 @@
+<?php
+namespace frontend\controllers;
+use yii\web\Controller;
+class CacheController extends Controller{
+	public function actionIndex(){
+		//缓存的几种方式：文件，nosql，mysql
+		//yii\yii2test\common\config\main.php
+		// return [
+  //   			'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+  //   		'components' => [
+  //       			'cache' => [
+  //           		'class' => 'yii\caching\FileCache',
+  //      			 ],
+  //   			],
+		// 	];
+
+		//调用cache组件
+		$cache=\YII::$app->cache; //cache->getCache()方法->方法中加载cache组件->cache文件
+		//添加数据
+		//$cache->add('key1','helloworld');
+		$cache->add('key1',"zhidao"); //往缓存中添加，如果键相同，那么添加的数据不会覆盖
+		//修改数据
+		$cache->set('key1','hello world2');
+		//删除数据
+		$cache->delete('key1');
+		//清空数据
+		$cache->flush();
+		//调出数据
+		var_dump($cache->get('key1'));
+
+
+	}
+
+}
+
+?>
