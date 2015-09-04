8ea5ec25e5efd0f06e9508a545a0652e5bf4da73
huangsen0912 <1417988198@qq.com>
2015-09-04 20:52:00 +0800

    查看是不是用了映射表（把路径写错）

--- yii2test/frontend/controllers/MapController.php
+++ yii2test/frontend/controllers/MapController.php
@@ -8,7 +8,7 @@ class MapController extends Controller{
 		//类的映射表
 		//new Order(不认识)->lazy_loading->autoload('order')->找到全名\app\models\Order->加载绝对路径文件F:/wamp/www/yii2/yii2test/frontend/models/Order.php
 		//如此加载也很耗时，那么如果能给他一个映射表，让它直接去找那个文件，就会提高效率
-		\Yii::$classMap['app\models\Order']='F:\wamp\www\yii2\yii2test\frontend\models\Order.php';
+		\Yii::$classMap['app\models\Order']='F:\wamp\www1\yii2\yii2test\frontend\models\Order.php';
 
 		//$order = new Order();
 		$order = new Order;
