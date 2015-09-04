e94aebbac25d2d3042d4643ec4f3bd8ac1c9205e
huangsen0912 <1417988198@qq.com>
2015-09-04 20:51:02 +0800

    映射表  $classMap数组

---
+++ yii2test/frontend/controllers/MapController.php
@@ -0,0 +1,19 @@
+<?php
+namespace frontend\controllers;
+use yii\web\Controller;
+use app\models\Order;
+class MapController extends Controller{
+
+	public function actionIndex(){
+		//类的映射表
+		//new Order(不认识)->lazy_loading->autoload('order')->找到全名\app\models\Order->加载绝对路径文件F:/wamp/www/yii2/yii2test/frontend/models/Order.php
+		//如此加载也很耗时，那么如果能给他一个映射表，让它直接去找那个文件，就会提高效率
+		\Yii::$classMap['app\models\Order']='F:\wamp\www\yii2\yii2test\frontend\models\Order.php';
+
+		//$order = new Order();
+		$order = new Order;
+
+	}
+}
+
+?>
